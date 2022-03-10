3497e7dabd6ea4d2a5d58c5d9a92db5e0e26cd9f
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-10 11:40:21 -0500

    ADD: Implementation of fizzbuzz method on solver class.

--- solver.rb
+++ solver.rb
@@ -16,6 +16,20 @@ class Solver
   end
   
   def fizzbuzz(number)
-    
+    if !number.is_a?(Numeric)
+      return 'Invalid input type: number must be an number.'
+    elsif !number.is_a?(Integer) && !number.positive?
+      return 'Invalid input type: number must be a positive integer.'
+    else
+      if number % 3 == 0 && number % 5 == 0
+        return 'fizzbuzz'
+      elsif number % 3 == 0
+        return 'fizz'
+      elsif number % 5 == 0
+        return 'buzz'
+      else
+        return number.to_s
+      end
+    end
   end
 end
